a916d4439c9377eeb90233c78f84994191744243
hingston <26564456+hingston@users.noreply.github.com>
2019-04-20 16:54:10 +0930

    Working with app engine

--- hingston/urls.py
+++ hingston/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
+from django.urls import path, include
 
 urlpatterns = [
-
+    path('', lambda x: HttpResponse("Home page!", content_type="text/plain")),
+    path('name/', include('name.urls'))
 ]
---
+++ main.py
@@ -0,0 +1,3 @@
+from hingston.wsgi import application
+
+app = application  # App engine looks here for the correct entry point (app)
