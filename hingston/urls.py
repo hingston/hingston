76d4ffd723b97be40b4feaf86c0e66f93ac8488c
hingston <26564456+hingston@users.noreply.github.com>
2019-04-20 17:08:33 +0930

    Setup homepage

--- hingston/urls.py
+++ hingston/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.http import HttpResponse
 from django.urls import path, include
 
+from hingston import views
+
 urlpatterns = [
-    path('', lambda x: HttpResponse("Home page!", content_type="text/plain")),
+    path('', views.index),
     path('name/', include('name.urls'))
 ]
